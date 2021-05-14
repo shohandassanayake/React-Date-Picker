6bd0c00557e7795a0fd25960feff45f01f6f3914
Shohan Dassanayake <shohan.clementdassanayake@kronos.com>
2021-05-14 16:47:23 +0530

    updated day component

--- src/components/day-view/day/day.tsx
+++ src/components/day-view/day/day.tsx
@@ -3,8 +3,12 @@ import React from 'react';
 import './day.scss'
 
 export default function Day(props): JSX.Element {
+    const onDayClick = () =>{
+        props.cell.date.setDate(props.cell.day);
+        props.setDate(props.cell.date)
+    }
     return (       
-        <div key={props.cell.index} className={`cell ${props.cell.dayType}`} >
+        <div key={props.cell.index} className={`cell ${props.cell.dayType}`} onClick={onDayClick}>
             <div className={`cell-inner ${props.cell.workingStatus}`}>                                    
                 
             </div>
