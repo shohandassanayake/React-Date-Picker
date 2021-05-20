0379092f6df430487513a7f106b0cf4338d0f621
shohandassanayake <shohan.cd@gmail.com>
2021-05-21 01:30:31 +0530

    Month Picker Update

--- src/components/month-view/month-view.tsx
+++ src/components/month-view/month-view.tsx
@@ -5,7 +5,8 @@ import CalendarUtil from '../../shared/util'
 import './month-view.scss'
 
 export default function MonthView(props): JSX.Element {
-    
+    const width = props.width/3 - 8;
+    const height = props.width/4 - 8;
     const monthData =  CalendarUtil.getMonthsForCalendar();
 
     const onMonthChange = (month) => {
@@ -16,7 +17,7 @@ console.log('Month-View-Rendered')
     return (
             <div className='month-view'>
             {monthData.map((title: string, index : number) => 
-                <div key={index} className='month-cell' onClick={() => onMonthChange(index)}>                            
+                <div key={index} className='month-cell' onClick={() => onMonthChange(index)} style={{height:height , width: width}}>                            
                     <div className='month-item'>{title}</div>                                    
                 </div>
             )}
