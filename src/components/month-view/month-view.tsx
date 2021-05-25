90a223add593f93b27700455d073e5997b29fa15
shohandassanayake <shohan.cd@gmail.com>
2021-05-26 00:53:35 +0530

    Datepicker month cell update

--- src/components/month-view/month-view.tsx
+++ src/components/month-view/month-view.tsx
@@ -3,11 +3,25 @@ import {DateChangeType} from '../../shared/enums'
 import CalendarUtil from '../../shared/util'
 
 import './month-view.scss'
+import Month from '../../shared/models/month';
 
 export default function MonthView(props): JSX.Element {
-    const width = props.width/3 - 8;
-    const height = props.width/4 - 8;
+    const width = props.data.width/3 - 8;
+    const height = props.data.width/4 - 8;
     const monthData =  CalendarUtil.getMonthsForCalendar();
+
+    if(props.data.isMinDate){
+        let minMonth = props.data.minDate.getMonth()
+        for(let i = 0; i < minMonth;i++){
+            monthData[i].class = 'disable-month'
+        }
+    }
+    else if(props.data.isMaxDate){
+        let maxMonth = props.data.maxDate.getMonth()
+        for(let i = maxMonth; i < 12;i++){
+            monthData[i].class = 'disable'
+        }
+    }
 
     const onMonthChange = (month) => {
         props.onDateChange(DateChangeType.month, month)
@@ -16,9 +30,9 @@ export default function MonthView(props): JSX.Element {
 console.log('Month-View-Rendered')
     return (
             <div className='month-view'>
-            {monthData.map((title: string, index : number) => 
-                <div key={index} className='month-cell' onClick={() => onMonthChange(index)} style={{height:height , width: width}}>                            
-                    <div className='month-item'>{title}</div>                                    
+            {monthData.map((month: Month, index : number) => 
+                <div key={index} className={`month-cell ${month.class}`} onClick={() => onMonthChange(month.name)} style={{height:height , width: width}}>                            
+                    <div className='month-item'>{month.name}</div>                                    
                 </div>
             )}
         </div>          
