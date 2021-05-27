675cb4c498041ea6299f87be3f54ad6c6ee78332
shohandassanayake <shohan.cd@gmail.com>
2021-05-28 02:14:49 +0530

    enums files update

---
+++ src/shared/enums/enums.ts
@@ -0,0 +1,17 @@
+export enum ViewType {
+    day,
+    month,
+    year    
+}
+
+export enum DateChangeType {
+    day,
+    nextDay,
+    prevDay,
+    nextMonth,
+    prevMonth,
+    nextYear,
+    prevYear,
+    month,
+    year
+}
