96beee6d70a4ee78463f88567860ca77b0132b2e
shohandassanayake <shohan.cd@gmail.com>
2021-06-11 22:24:35 +0530

    Update Datepicker

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactDatePicker from './pages/date-picker/date-picker';
 ReactDOM.render(  
-  <ReactDatePicker/>,
+ 
+      <ReactDatePicker
+        width={400}
+        setDate={new Date()}
+        onChange={void 0}
+        startDayOfWeek={0}
+        minDate={new Date('2018-02-05')}
+        maxDate={new Date('2021-10-25')}
+        picker={'month'}
+      />,
+ 
   document.getElementById('root'),
-);
+);  
 
+//export * from './pages/date-picker/date-picker'
